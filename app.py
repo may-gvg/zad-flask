7fcaebe5526f922f429ffc9f08c85363500cdad6
may <83417030+may-gvg@users.noreply.github.com>
2021-06-08 15:43:58 +0200

    Update app.py
    zmiana treści kody pod zadanie

--- app.py
+++ app.py
@@ -5,25 +5,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    my_name = "John"
-    return f'Hello, {my_name}!'
-
-@app.route('/hello')
-def hello2():
-    my_name = "Maciek "
-    return f'Hello, {my_name}!'
-
-@app.route('/blog', methods=['GET'])
-def blog_main():
-    return f"This is a main blog page"
-
-@app.route('/blog/<id>')
-def blog(id):
-    return f"This is blog entry #{id}"
-
-
 @app.route('/greeting', methods=['GET', 'POST'])
 def message():
    if request.method == 'GET':
@@ -35,43 +16,37 @@ def message():
        return redirect("/")
 
 
-@app.route("/warehouse_ugly")
-def warehouse_ugly():
-    items = ["screwdriver", "hammer", "saw"]
-    html = "<ul>"
-    for item in items:
-        html = html + f"<li>{item}</li>"
-    html += "</ul>"
-    return html
-
-@app.route("/warehouse")
-def warehouse():
-    items = ["screwdriver", "hammer", "saw"]
-    errors = ["hammer is broken!"]
-    return render_template("greeting.html", items=items, errors=errors)
-
-
-
-@app.route ('/greeting', methods=['GET', 'POST'])
-def mypage():
+@app.route('/tata', methods=['GET', 'POST'])
+def tata():
    if request.method == 'GET':
        print("We received GET")
-       return render_template("greeting.html")
+       return render_template("parent.html")
    elif request.method == 'POST':
        print("We received POST")
        print(request.form)
        return redirect("/")
 
 
-@app.route ('/contact', methods=['GET', 'POST'])
-def contact():
+
+
+@app.route ('/mypage/me', methods=['GET', 'POST'])
+def mypage():
    if request.method == 'GET':
        print("We received GET")
-       return render_template("greeting.html")
+       return render_template("me.html")
    elif request.method == 'POST':
        print("We received POST")
        print(request.form)
-       return render_template("greeting.html")
+       return redirect("/")
 
 
+@app.route ('/mypage/contact', methods=['GET', 'POST'])
+def contact():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("contact.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return render_template("contact.html")
 
